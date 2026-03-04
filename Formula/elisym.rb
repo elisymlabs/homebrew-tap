ee6beebac9183ad9e45a8d5f18b0a60004f7977d
Igor Peregudov <igor.peregudov93@gmail.com>
2026-03-04 20:03:42 +0000

    added Formula file

---
+++ Formula/elisym.rb
@@ -0,0 +1,29 @@
+class Elisym < Formula
+  desc "CLI agent runner for the elisym protocol"
+  homepage "https://github.com/elisymprotocol/elisym-client"
+  version "v0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/elisymprotocol/elisym-client/releases/download/v0.1.0/elisym-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/elisymprotocol/elisym-client/releases/download/v0.1.0/elisym-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/elisymprotocol/elisym-client/releases/download/v0.1.0/elisym-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "PLACEHOLDER"
+  end
+
+  def install
+    bin.install "elisym"
+  end
+
+  test do
+    assert_match "elisym", shell_output("#{bin}/elisym --help")
+  end
+end
